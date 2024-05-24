82bceea7bed8f2ac43a281c69dfce935fe20f1bd
Rafiu Animashaun <rafiu.ani@gmail.com>
2024-05-24 01:35:11 -0500

    added signup feature

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,19 +1,32 @@
+const User = require("../model/user-model");
+
 const express = require("express");
 
 function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-function signup(req, res) {
-  userData = req.body;
-} 
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup();
+
+  res.redirect('/login')
+}
 
 function getLogin(req, res) {
-    res.render("customer/auth/login");
-  }
+  res.render("customer/auth/login");
+}
 
 module.exports = {
-    getSignup: getSignup,
-    getLogin: getLogin,
-    signup: signup,
-}
+  getSignup: getSignup,
+  getLogin: getLogin,
+  signup: signup,
+};
