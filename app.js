276fa29698a556f75a555488220db147e46d081b
Rafiu Animashaun <rafiu.ani@gmail.com>
2024-05-24 02:28:12 -0500

    added session, default error handling, and csrf protection

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const path = require("path");
 
 const express = require("express");
+const csrf = require("csurf");
+const expressSession = require("express-session");
 
+const createSessionConfig = require("./config/session");
 const db = require("./data/database");
+const addCSRFTokenMiddleware = require("./middleware/csrf-token");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 const authRoutes = require("./routes/auth-routes");
 
 const app = express();
@@ -13,7 +18,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(addCSRFTokenMiddleware);
+
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
